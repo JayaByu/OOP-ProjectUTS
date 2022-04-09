2990c49c36712725df2e03a0d4c3bd7910112126
JayaByu <jayawardhanabayu@gmail.com>
2022-04-09 21:41:06 +0700

    add eMoney.class

--- src/eMoney.java
+++ src/eMoney.java
@@ -1,8 +1,36 @@
-
 public class eMoney {
+    private String noVA;
+    private int Saldo;
+    private dataUser dataUser;
 
-	public eMoney() {
-		// TODO Auto-generated constructor stub
-	}
+    public eMoney(String noVA, int saldo, dataUser dataUser) {
+        this.noVA = noVA;
+        Saldo = saldo;
+        this.dataUser = dataUser;
+    }
+
+    public dataUser getDataUser() {
+        return dataUser;
+    }
+
+    public void setDataUser(dataUser dataUser) {
+        this.dataUser = dataUser;
+    }
+
+    public String getNoVA() {
+        return noVA;
+    }
+
+    public void setNoVA(String noVA) {
+        this.noVA = noVA;
+    }
+
+    public int getSaldo() {
+        return Saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        Saldo = saldo;
+    }
 
 }
